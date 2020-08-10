6fb0201953f40e4b92d5bca79143eaa2f63ae982
x4kkk3r <x4kkk3r@github.com>
2020-08-10 08:00:01 +0000

    More Vector tests

---
+++ tests/TUVector2.cpp
@@ -0,0 +1,80 @@
+#include "../include/IOMath/extended/vectors/UVector2.hpp"
+#include "VectorAsserts.hpp"
+
+using namespace IOMath;
+
+void TestZeroVector()
+{
+    UVector2 zeroVector = UVector2();
+    AssertVector(zeroVector, 0u);
+}
+
+void TestScalarVector()
+{
+    UVector2 scalarVector = UVector2(3u);
+    AssertVector(scalarVector, 3u);
+}
+
+void TestVectorAdd()
+{
+    UVector2 firstAdd = UVector2(1u, 2u);
+    UVector2 secondAdd = UVector2(5u, 4u);
+
+    AssertVector(firstAdd + secondAdd, 6u, 6u);
+}
+
+void TestVectorSubtract()
+{
+    UVector2 minuend = UVector2(5u, 11u);
+    UVector2 subtrahend = UVector2(1u, -11u);
+
+    AssertVector(minuend - subtrahend, 4u, 22u);
+}
+
+void TestVectorMultiply()
+{
+    UVector2 firstMultiplier = UVector2(5u, 3u);
+    UVector2 secondMultiplier = UVector2(10u, 2u);
+    
+    AssertVector(firstMultiplier * secondMultiplier, 50u, 6u);
+}
+
+void TestVectorDivide()
+{
+    UVector2 dividend = UVector2(1u, 7u);
+    UVector2 divider = UVector2(-1u, 2u);
+
+    AssertVector(dividend / divider, -1u, 3u);
+}
+
+void TestVectorBracketsOperator()
+{
+    UVector2 vector = UVector2(7u, 3u);
+
+    AssertVector(vector, vector[0], vector[1]);
+}
+
+void TestVectorFromOther()
+{
+    UVector2 other = UVector2(1u, 2u);
+    UVector2 vector = UVector2(other);
+
+    AssertVector(vector, other);
+}
+
+void Test()
+{
+    TestZeroVector();
+    TestScalarVector();
+    TestVectorAdd();
+    TestVectorSubtract();
+    TestVectorMultiply();
+    TestVectorDivide();
+    TestVectorBracketsOperator();
+}
+
+int main()
+{
+    Test();
+    return 0;
+}
---
+++ tests/TestUVectors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+g++ tests/TUVector2.cpp -std=c++17
+./a.out
+g++ tests/TUVector3.cpp -std=c++17
+./a.out
+g++ tests/TUVector4.cpp -std=c++17
+./a.out
